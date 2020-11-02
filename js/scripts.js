42dab8fe98b422d943dcd966dd706eff8a2d79a2
Msg89 <mariasglynou@gmail.com>
2020-11-02 12:51:45 +0200

    Fix: input 0 error, accessibility, reset button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,47 @@
+//Calculate BMI for user data & change the color of the table.
 function getBMI(){
-    var kg = document.querySelector('#user-kg').value;
-    var cm = document.querySelector('#user-cm').value;
-    
+  var kg = document.querySelector('#user-kg').value;
+  var cm = document.querySelector('#user-cm').value;
+  
+  //Will not let user insert numbers <0.
+  if (kg <=0 || cm <=0){
+    window.alert ("Please insert values higher than 0.");
+  } else {
     var bmi = (kg/(cm*cm))*10000;
-   var calc = document.getElementById("change").innerHTML = Math.round(bmi*100)/100;
-    
-    if (calc < 18.5){
-   document.querySelector("#underweight").style.backgroundColor="red";
+    var calc = document.getElementById('user-bmi').innerHTML = Math.round(bmi*100)/100;
+  }
+  
+  //Change the color of the table depending on the BMI results.
+  if (calc < 18.5){
+      document.querySelector('#underweight').style.backgroundColor="red";
     } else if(calc>=18.5 && calc<24.9){
-      document.querySelector("#healthy-weight").style.backgroundColor="green";
-    } else if(calc>=24.9 && calc<29.9){                     document.querySelector("#overweight").style.backgroundColor="yellow";
+      document.querySelector('#healthy-weight').style.backgroundColor="green";
+    } else if(calc>=24.9 && calc<29.9){                     
+      document.querySelector('#overweight').style.backgroundColor="yellow";
     } else if(calc>=29.9 && calc<34.9){
-      document.querySelector("#obese").style.backgroundColor="orange";
+      document.querySelector('#obese').style.backgroundColor="orange";
     }else if(calc>=34.9 && calc<39.9){
-      document.querySelector("#severely-obese").style.backgroundColor="red";
+      document.querySelector('#severely-obese').style.backgroundColor="red";
     }else if(calc>=39.9){
-      document.querySelector("#morbidly-obese").style.backgroundColor="purple";
+      document.querySelector('#morbidly-obese').style.backgroundColor="purple";
     }
   }
   
-   
+  
+// Reset properties: input fields, bmi result and table background color.
 function reset(){
-     document.querySelector("#underweight").style.backgroundColor="rgb(232, 228, 247)";
-     document.querySelector("#healthy-weight").style.backgroundColor="rgb(232, 228, 247)";
-     document.querySelector("#overweight").style.backgroundColor="rgb(232, 228, 247)";
-     document.querySelector("#obese").style.backgroundColor="rgb(232, 228, 247)";
-     document.querySelector("#severely-obese").style.backgroundColor="rgb(232, 228, 247)";
-     document.querySelector("#morbidly-obese").style.backgroundColor="rgb(232, 228, 247)";
+  // Reset properties by setting them to an empty string.
+  document.getElementById('user-kg').value = '';
+  document.getElementById('user-cm').value = '';
+  
+  //Reset bmi result
+  document.querySelector('#user-bmi').innerHTML="...";
+
+  //Reset background color for table
+  document.querySelector('#underweight').style.backgroundColor="";
+  document.querySelector('#healthy-weight').style.backgroundColor="";
+  document.querySelector('#overweight').style.backgroundColor="";
+  document.querySelector('#obese').style.backgroundColor="";
+  document.querySelector('#severely-obese').style.backgroundColor="";
+  document.querySelector('#morbidly-obese').style.backgroundColor="";
 }
